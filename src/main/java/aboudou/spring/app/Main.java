34ea18c2c723524f605dd49a0893ff1b098c09b9
Sare Samadou <saresamadou1991@gmail.com>
2016-11-27 18:56:35 +0100

    Configure bean using p namespace

--- src/main/java/aboudou/spring/app/Main.java
+++ src/main/java/aboudou/spring/app/Main.java
@@ -7,20 +7,26 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		//Get instance of application context
 		ApplicationContext context = new FileSystemXmlApplicationContext("appContext.xml");
 		
+		//Get bean from the context
 		Fruit fruit = context.getBean("fruit", Fruit.class);
 		Vegetable vegtable = context.getBean("vegetable", Vegetable.class);
 		Fruit fruitwithName = context.getBean("fruitwithName", Fruit.class);
 		Vegetable vegtablewithName = context.getBean("vegetablewithName", Vegetable.class);
 		Fruit fruitWithListAndMap = (Fruit)context.getBean("fruitWithListAndMap");
+		Vegetable vegUsingPNameSpace = context.getBean("vegUsingPNameSpace", Vegetable.class);
 		
+		//Play with the bean
 		System.out.println(fruit.talkAboutYourself());
 		System.out.println(vegtable.talkAboutYourself());
 		System.out.println(fruitwithName.talkAboutYourself());
 		System.out.println(vegtablewithName.talkAboutYourself());
 		System.out.println(fruitWithListAndMap.talkAboutYourself());
+		System.out.println(vegUsingPNameSpace.talkAboutYourself());
 		
+		//Close the instance of application context
 		((FileSystemXmlApplicationContext) context).close();
 		
 	}
