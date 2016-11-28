5e7b31eb2df17c3f84b3a35d23471ceaf6427ca6
Sare Samadou <saresamadou1991@gmail.com>
2016-11-28 13:31:17 +0100

    Create a beans by reading prperties from external file

--- src/main/java/aboudou/spring/app/Fruit.java
+++ src/main/java/aboudou/spring/app/Fruit.java
@@ -3,39 +3,47 @@ package aboudou.spring.app;
 import java.util.List;
 import java.util.Map;
 
-
 public class Fruit {
 
-	//Class variables
-	
+	// Class variables
+
 	private String myName;
-	
+
+	private String definition = "not set !";
+
 	private List<String> fruitNameList;
 
 	private Map<String, String> fruitNameMap;
-	
-	//Default no args constructor
+
+	// Default no args constructor
 	public Fruit() {
 
 	}
 
-	//Constuctor with args
+	// Constuctor with args
 	public Fruit(String myName) {
 		super();
 		this.myName = myName;
 	}
 
-	//Getters and setters
-	
+	// Getters and setters
+
 	public String getMyName() {
 		return myName;
+	}
+
+	public String getDefinition() {
+		return definition;
+	}
+
+	public void setDefinition(String definition) {
+		this.definition = definition;
 	}
 
 	public void setMyName(String myName) {
 		this.myName = myName;
 	}
 
-	
 	public List<String> getFruitNameList() {
 		return fruitNameList;
 	}
@@ -52,39 +60,42 @@ public class Fruit {
 		this.fruitNameMap = fruitNameMap;
 	}
 
-	//ToString Method
+	// ToString Method
 	@Override
 	public String toString() {
 		return "Fruit [myName=" + myName + "]";
 	}
 
-	//Method that Fruit used to talk about himself
+	// Method that Fruit used to talk about himself
 	public String talkAboutYourself() {
 		String speech = "Hi I am a fruit";
-		
-		//Give the name only if the name is set
+
+		// Give the name only if the name is set
 		if (this.myName != null && this.myName != "") {
 			speech += " and my name is " + this.myName;
 		}
-		
-		//Show the fruit list names if list exist
-		if(fruitNameList!=null && fruitNameList.size() > 0){
-			speech+="\n List: ";
+
+		// Show the fruit list names if list exist
+		if (fruitNameList != null && fruitNameList.size() > 0) {
+			speech += "\n List: ";
 			for (String fruitName : fruitNameList) {
-				speech+= " name " + fruitName;
+				speech += " name " + fruitName;
 			}
 		}
-		
-		//Show the fruit map names if list exist
-		if(fruitNameMap!=null && fruitNameMap.size() > 0){
-			speech+="\n Map: ";
+
+		// Show the fruit map names if list exist
+		if (fruitNameMap != null && fruitNameMap.size() > 0) {
+			speech += "\n Map: ";
 			for (Map.Entry<String, String> fruitMap : fruitNameMap.entrySet()) {
-				speech+=fruitMap.getKey() + " ==> "+ fruitMap.getValue() + " :: ";
+				speech += fruitMap.getKey() + " ==> " + fruitMap.getValue() + " :: ";
 			}
 		}
-		
-		
- 		return speech;
+
+		return speech;
+	}
+	
+	public String talkAboutYourDefinition() {
+		return this.definition;
 	}
 
 }
--- src/main/java/aboudou/spring/app/Main.java
+++ src/main/java/aboudou/spring/app/Main.java
@@ -17,6 +17,7 @@ public class Main {
 		Vegetable vegtablewithName = context.getBean("vegetablewithName", Vegetable.class);
 		Fruit fruitWithListAndMap = (Fruit)context.getBean("fruitWithListAndMap");
 		Vegetable vegUsingPNameSpace = context.getBean("vegUsingPNameSpace", Vegetable.class);
+		Meal meal =(Meal)context.getBean("mealUsingConstructorInj");
 		
 		//Play with the bean
 		System.out.println(fruit.talkAboutYourself());
@@ -24,10 +25,14 @@ public class Main {
 		System.out.println(fruitwithName.talkAboutYourself());
 		System.out.println(vegtablewithName.talkAboutYourself());
 		System.out.println(fruitWithListAndMap.talkAboutYourself());
-		System.out.println(vegUsingPNameSpace.talkAboutYourself());*/
+		System.out.println(vegUsingPNameSpace.talkAboutYourself());
+		System.out.println(meal.whatsInThisMeal());*/
 		
-		Meal meal =(Meal)context.getBean("mealUsingConstructorInj");
-		System.out.println(meal.whatsInThisMeal());
+		
+		Fruit fruit = context.getBean("externalFruit", Fruit.class);
+		Vegetable vegtable = context.getBean("vegetable", Vegetable.class);
+		System.out.println(vegtable.talkAboutYourself());
+		System.out.println(fruit.talkAboutYourDefinition());
 		
 		//Close the instance of application context
 		((FileSystemXmlApplicationContext) context).close();
